aa1ac6d1607365752866548c7f59e7a99546c569
Thiago Bignotto <bignotto@gmail.com>
2021-05-01 15:37:11 -0300

    Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
+import Head from "next/head";
+import styles from "./home.module.scss";
+
 export default function Home() {
-  return <h1>Hello Big World from NextJS</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home - ig.News</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all publications for <br />
+            <span>$9.90/month.</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Coding Girl" />
+      </main>
+    </>
+  );
 }
