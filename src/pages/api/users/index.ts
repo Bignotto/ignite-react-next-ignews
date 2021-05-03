4ff2ab72618966767825c339b3bd8621fb238912
Thiago Bignotto <bignotto@gmail.com>
2021-05-02 23:26:31 -0300

    Next API folder example

---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  const users = [
+    { id: 1, name: "Thiago" },
+    { id: 2, name: "Damaris" },
+    { id: 3, name: "Guilherme" },
+    { id: 4, name: "Pedro" },
+    { id: 5, name: "André" },
+    { id: 6, name: "Gabriel" },
+    { id: 7, name: "Felipe" },
+    { id: 8, name: "Sérgio" },
+    { id: 9, name: "Gelson" },
+    { id: 10, name: "Vitor" },
+    { id: 11, name: "Edgard" },
+  ];
+
+  return response.json(users);
+};
