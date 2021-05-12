f58b2aee016962d14c7cfc0fa204c6eb459fb6a3
Thiago Bignotto <bignotto@gmail.com>
2021-05-12 06:28:39 -0300

    Formatting and listing posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,8 +4,20 @@ import { getPrismicClient } from "../../services/prismic";
 import Prismic from "@prismicio/client";
 
 import styles from "./styles.module.scss";
+import { RichText } from "prismic-dom";
 
-export default function Posts() {
+interface Post {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -14,46 +26,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>24 Maio 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a monorepo to manage
-              multiple pagckages with a shared build, test and release process.
-            </p>
-          </a>
-          <a href="#">
-            <time>24 Maio 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a monorepo to manage
-              multiple pagckages with a shared build, test and release process.
-            </p>
-          </a>
-          <a href="#">
-            <time>24 Maio 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a monorepo to manage
-              multiple pagckages with a shared build, test and release process.
-            </p>
-          </a>
-          <a href="#">
-            <time>24 Maio 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a monorepo to manage
-              multiple pagckages with a shared build, test and release process.
-            </p>
-          </a>
-          <a href="#">
-            <time>24 Maio 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a monorepo to manage
-              multiple pagckages with a shared build, test and release process.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -71,9 +50,27 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
-  console.log(response);
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
